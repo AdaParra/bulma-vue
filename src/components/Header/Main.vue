dbfecb1a3753f8797dab49dddaf929e308a4cbd3
AdaParra <adapparrad@gmail.com>
2017-10-31 19:26:27 -0400

    Creando repositorio

---
+++ src/components/Header/Main.vue
@@ -0,0 +1,75 @@
+<template>
+<div>  
+    <div class="container is-fullhd">
+      <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <img src="../../assets/logo-4.png" width="160px" alt="Bulma and vuejs">
+          </a>
+          <button class="button navbar-burger" :data-target="dataTarget">
+            <span> </span>
+            <span> </span>
+            <span> </span>
+          </button>
+        </div>
+
+        <div class="navbar-menu" :id="idnavMenu">
+          
+          <div class="navbar-start">
+
+              <router-link :to="{ name: 'Home'}" class="navbar-item">
+                Inicio
+              </router-link>
+
+              <router-link :to="{ name: 'Projects' }" class="navbar-item">
+                Proyectos
+              </router-link>
+
+              <a class="navbar-item">
+                ¿Como Funciona?
+              </a>
+
+              <a class="navbar-item">
+                Preguntas Frecuentes
+              </a>
+          </div>
+      
+        <div class="navbar-end">
+            <a class="navbar-item">
+              <div class="buttons">
+                <span class="button is-success">Registrate</span>
+              </div>
+            </a>
+            <a class="navbar-item">
+              <div class="buttons">
+                <span class="button is-info">Iniciar Sesión</span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+</div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+  @import '../../scss/main.scss'
+
+</style>
+
+<style type="text/css">
+
+    .container-logo{
+        background: #004892;
+        height: 57px;
+        width: 180px;
+        margin-right: 30px;
+    }
+
+    .navbar-item{
+        padding-top: 0px;
+    }
+ 
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div>
+        <Header></Header>
+
+        <Footer></Footer>
+    </div>
+    
+</template>
+
+<script>
+
+import Header from './Header/Main.vue'
+import Footer from './Footer/Main.vue'
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Home'
+    }
+  },
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+<style type="scss">
+
+    @import '../scss/main.scss'
+
+</style>
